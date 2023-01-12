3a47bb13ecfd929b597ae06bc14e0ad03a8bb7a5
Kyle <kylegruber62@gmail.com>
2023-01-11 23:28:56 -0500

    finished the generateMarkdown.js
    cleaned it out
    put in the README info

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,59 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+ const licenseBadgeLinks = {
+  MIT: `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`,
+  Mozzilla: `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`,
+  Perl: `[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)`,
+  Unlicense: `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`,
+};
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function READMEOutput(data) {
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+// data.licenseBadge = licenseBadgeLinks[data.license];
+
+`# ${data.title}
+${data.licenseBadge}
+
+## Description
+
+${data.description}
+
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribution Guidelines](#contributionsGuidelines)
+* [License](#license)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+
+${data.install}
+## Usage
+
+${data.usage}
+
+## Contriubtion Guidelines
+
+${data.contribution}
+
+## License
+
+Click here to see license website ${data.licenseBadge}
+
+## »»————-　★　————-««
+
+
+
+##Questions
+
+${data.github}
+For Queastions please contact me at [${data.email}](mailto:${data.email}).;
+
 
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = READMEOutput;
